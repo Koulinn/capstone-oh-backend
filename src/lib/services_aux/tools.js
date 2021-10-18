bd1dfc2dd12f6b778e17b4173e9db7a149fb88c5
Koulinn <drdverzola@gmail.com>
2021-10-18 15:43:23 +0100

    checkSchema error function

--- src/lib/services_aux/tools.js
+++ src/lib/services_aux/tools.js
@@ -1,15 +1,33 @@
-import {getAge} from "get-complete-age"
+import { getAge } from "get-complete-age"
+import {validationResult } from "express-validator";
+import createError from "http-errors";
 
 
 // date must be mm-dd-yyyy
-const getPreciseAge = (date) =>{
+const getPreciseAge = (date) => {
     return getAge.getAge(date)
 
 }
 
+const checkSchemaErrors = (req, res, next) => {
+    try {
+             
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            next(createError(400, "Validation error", errors))
+        } else {
+            next()
+        }
+    } catch (error) {
+        console.log(error)
+        next()
+    }
+}
 
-const tools ={
-    getPreciseAge:getPreciseAge
+
+const tools = {
+    getPreciseAge: getPreciseAge,
+    checkSchemaErrors: checkSchemaErrors
 }
 
 export default tools
