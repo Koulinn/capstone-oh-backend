22bfd7fb9d2008e9dd603a77af71e6fe8e400699
Koulinn <drdverzola@gmail.com>
2021-10-19 19:34:14 +0100

    created parseJSON, isRegisteredUser, createPreDefinedUser, definedUserAvailability
    auxiliary functions for the booking process

--- src/lib/services_aux/tools.js
+++ src/lib/services_aux/tools.js
@@ -1,6 +1,8 @@
 import { getAge } from "get-complete-age"
 import { validationResult } from "express-validator";
 import createError from "http-errors";
+import { verifyJWT } from "../auth/jwt-aux.js";
+import UserModel from '../../db/Schemas/User.js'
 
 
 // date must be mm-dd-yyyy
@@ -46,10 +48,52 @@ const createUserMedTestsList = (req) => {
     return allMedicalRequests
 }
 
-const parseUserAvailability = async (req)=>{
-    const userAvailability = await JSON.parse(req.body.userAvailability)
-    console.log(userAvailability)
-    return userAvailability
+const parseJSON = async (req) => {
+    const parsedJSON = await JSON.parse(req)
+    return parsedJSON
+}
+
+const isRegisteredUser = async (req, res, next) => {
+    try {
+        if (req.headers.authorization) {
+            const token = req.headers.authorization.replace("Bearer ", "")
+            const decodedToken = await verifyJWT(token)
+            const user = await UserModel.findById(decodedToken._id)
+            req.user = user
+        } 
+        
+        next()
+        
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+const createPreDefinedUser = async (req, res, next) => {
+    try {
+        if (req.user) {
+            next()
+        } else {
+            const parsedUser = await parseJSON(req.body.preDefinedUser)
+            const newUser = new UserModel(parsedUser)
+            const savedUser = await newUser.save({ new: true })
+            req.user = savedUser
+            next()
+        }
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+const definedUserAvailability = async (req)=>{
+    const userAvailability = await tools.parseJSON(req)
+    if(userAvailability[0].hasOwnProperty('ASAP')){
+      return userAvailability[0]
+    } else {
+      return userAvailability
+    }
   }
 
 
@@ -57,7 +101,10 @@ const tools = {
     getPreciseAge: getPreciseAge,
     checkSchemaErrors: checkSchemaErrors,
     createUserMedTestsList: createUserMedTestsList,
-    parseUserAvailability:parseUserAvailability
+    parseJSON: parseJSON,
+    isRegisteredUser: isRegisteredUser,
+    createPreDefinedUser: createPreDefinedUser,
+    definedUserAvailability:definedUserAvailability
 }
 
 export default tools
