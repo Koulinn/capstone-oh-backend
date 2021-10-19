bd14be743e6441223c18519fcf2e56875eeb20df
Koulinn <drdverzola@gmail.com>
2021-10-19 15:02:53 +0100

    created createUserMedTestsList
    to deal with request

--- src/lib/services_aux/tools.js
+++ src/lib/services_aux/tools.js
@@ -1,5 +1,5 @@
 import { getAge } from "get-complete-age"
-import {validationResult } from "express-validator";
+import { validationResult } from "express-validator";
 import createError from "http-errors";
 
 
@@ -11,7 +11,7 @@ const getPreciseAge = (date) => {
 
 const checkSchemaErrors = (req, res, next) => {
     try {
-             
+
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
             next(createError(400, "Validation error", errors))
@@ -25,9 +25,32 @@ const checkSchemaErrors = (req, res, next) => {
 }
 
 
+const createUserMedTestsList = (req) => {
+    let allMedicalRequests = {
+        userTags: [],
+        userFilesURL: [],
+    }
+    const tags = req.body.medicalRequestsTags
+    const requestsUrls = req.files.medicalRequestsImgs
+
+    //for images/files
+    Array.isArray(requestsUrls) ? allMedicalRequests.userFilesURL = [...requestsUrls.map(request => request.path)] : ''
+
+    //multiples tags
+    Array.isArray(tags) ? allMedicalRequests.userTags = [...tags] : ''
+    //single tags
+    tags && !Array.isArray(tags) ? allMedicalRequests.userTags = [tags] : ''
+
+
+
+    return allMedicalRequests
+}
+
+
 const tools = {
     getPreciseAge: getPreciseAge,
-    checkSchemaErrors: checkSchemaErrors
+    checkSchemaErrors: checkSchemaErrors,
+    createUserMedTestsList: createUserMedTestsList
 }
 
 export default tools
