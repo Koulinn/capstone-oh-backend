aef4a093a740b49ab335762828ed1db7daef4c04
Koulinn <drdverzola@gmail.com>
2021-10-22 09:16:23 +0100

    established socket connection

--- src/server/index.js
+++ src/server/index.js
@@ -9,6 +9,7 @@ import userRouter from './services/user/index.js'
 import hospitalRouter from './services/hospital/index.js'
 import errorHandlers from "./server_aux/error_handlers.js"
 import googleStrategy from "../lib/auth/Oauth/strategy-config.js"
+import { connectSocket } from "./socket/index.js"
 
 
 
@@ -42,6 +43,7 @@ if((process.env.MONGO_DEV_URL) || (process.env.MONGO_PROD_URL)){
 httpServer.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`)
 })
+    connectSocket(server)
     connectToDB()
     server.on("error", (error) =>
         console.log("🚀 Server is crashed due to ", error)
