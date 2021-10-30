794dfb1595913679798827df8f83a6f27de52f6e
Koulinn <drdverzola@gmail.com>
2021-10-30 07:36:53 +0100

    prepared partially the socket.io connection

--- src/server/socket/index.js
+++ src/server/socket/index.js
@@ -3,7 +3,7 @@ import { httpServer } from '../index.js'
 import socketHandlers from './socket-handlers.js'
 
 
-const {newUserConnection} = socketHandlers
+const {newUserConnection, dcUser} = socketHandlers
 
 let waitingUsers = []
 let assistants = []
@@ -15,38 +15,54 @@ export const connectSocket = (server) => {
         const io = new Server(httpServer, { allowEIO3: true })
 
         io.on('connection', socket => {
-            console.log('inside connection')
-
+            console.log('Coon established')
+            console.log(waitingUsers.length, 'from forceDC')
             socket.on('newUser', async (payload) => {
-                await newUserConnection(payload)
-                socket.to(assistantsRoom).emit('newUserOn', payload.user)
-                console.log('inside NEW user')                
+                const user = payload.user
+                await newUserConnection(payload, waitingUsers, socket.id)
+                socket.emit('newUserOn', user)
+                socket.join(user._id)            
             })
             socket.on('newAssistant', async (payload) => {
-                //    add to list of online assistants
-                //    EMIT waitingUsers: send the list of online users
+                const newAssistant ={
+                    assistant: payload.assistant,
+                    socketID: socket.id
+                }
+                assistants.push(newAssistant)
+                socket.emit('waitingUsers', waitingUsers)
             })
             socket.on('openRoomWithUser', async (payload) => {
-                //    assistant get the first index with flag isWaiting = true
-                // create room to the DB
-                // open JOIN room with this user
+                const assistant = payload
+                const {user} = waitingUsers.shift()
+                console.log(payload, 'user id from openroom')
+                socket.join(user._id)
+                socket.emit("onUserChat", user)
+                socket.to(user._id).emit('onChat',assistant)
+
+                // save room in DB
+                
                 // open EMIT notification to the user and assistant that hey are in the same room
                 // User is sent from waiting interface to the chat interface
             })
             socket.on('newMessage', async (payload) => {
+                console.log('inside new message >>>room ID', payload.roomID)
+                const {roomID} = payload
                 // save message to the DB
                 // message can have multiples files
+                socket.to(roomID).emit('recipientMessage', payload)
                 // EMIT to the assistant the user message
                 // EMIT to the user that the assistant has received a message
             })
 
-            socket.on('forceDisconnect', () => {
-                // onlineUsers = onlineUsers.filter(onlineUser => onlineUser.socketId !== socket.id)
+            socket.on('forceUserDisconnect', () => {
+               dcUser(waitingUsers, socket) 
+               socket.emit('updateWaitingUser', waitingUsers)
                 console.log(waitingUsers.length, 'from forceDC')
 
             })
             socket.on("disconnect", () => {
-                // onlineUsers = onlineUsers.filter(onlineUser => onlineUser.socketId !== socket.id)
+                dcUser(waitingUsers, socket) 
+                socket.emit('updateWaitingUser', waitingUsers)
                 console.log(waitingUsers.length, 'from disconnect')
             })
         })
