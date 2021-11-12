24fa861878b24cd17c2918a17a36cd2d39796a22
Koulinn <drdverzola@gmail.com>
2021-11-12 11:15:46 +0000

    testing pdf

--- src/lib/email/index.js
+++ src/lib/email/index.js
@@ -19,7 +19,8 @@ sgMail.setApiKey(apiKey)
 
 export const createPDFOnDisk = async (user, request) => {
     const createSyncPipeline = promisify(pipeline)
-    const path = join(dirname(fileURLToPath(import.meta.url)), `./pdfs/${request._id}.pdf`)
+    // const path = join(dirname(fileURLToPath(import.meta.url)), `./pdfs/${request._id}.pdf`)
+    const path = `./pdfs/${request._id}.pdf`
     const pdfStream = await createPDFReadableStream(user, request)
     await createSyncPipeline(pdfStream, fs.createWriteStream(path))
     return path
