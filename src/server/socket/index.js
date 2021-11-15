98f2d0b4c37a8a9731873e514f2f25d60dc12d7c
Koulinn <drdverzola@gmail.com>
2021-11-15 10:54:48 +0000

    prettier and removed console.log

--- src/server/socket/index.js
+++ src/server/socket/index.js
@@ -1,108 +1,98 @@
-import { Server } from 'socket.io'
-import { httpServer } from '../index.js'
-import socketHandlers from './socket-handlers.js'
+import { Server } from "socket.io";
+import { httpServer } from "../index.js";
+import socketHandlers from "./socket-handlers.js";
 
+const { newUserConnection, dcUser, saveRoom, isExistentRoom, saveMessage } =
+  socketHandlers;
 
-
-const { newUserConnection, dcUser, saveRoom, isExistentRoom, saveMessage } = socketHandlers
-
-let waitingUsers = []
-let assistants = []
-const assistantsRoom = 'assistRoom'
+let waitingUsers = [];
+let assistants = [];
+const assistantsRoom = "assistRoom";
 
 export const connectSocket = (server) => {
-    try {
+  try {
+    const io = new Server(httpServer, { allowEIO3: true });
 
-        const io = new Server(httpServer, { allowEIO3: true })
+    io.on("connection", (socket) => {
+      socket.on("newUser", async (payload) => {
+        await newUserConnection(payload, waitingUsers, socket.id);
+        assistants.forEach((a) => {
+          socket.to(a.assistant._id).emit("waitingUsers", waitingUsers);
+        });
+        socket.join(payload._id);
+      });
+      socket.on("newAssistant", async (payload) => {
+        const newAssistant = {
+          assistant: payload,
+          socketID: socket.id,
+        };
+        const isAssistantOn = assistants.find(
+          (a) => a.assistant._id === payload._id
+        );
+        if (assistants.length > 0) {
+          assistants.push(newAssistant);
+          socket.join(payload._id);
+          socket.emit("waitingUsers", waitingUsers);
+          return;
+        }
 
-        io.on('connection', socket => {
-            console.log(waitingUsers.length, ' number of waiting users connection')
-            socket.on('newUser', async (payload) => {
+        if (isAssistantOn) {
+        } else {
+          assistants.push(newAssistant);
+        }
 
+        socket.join(payload._id);
+        socket.emit("waitingUsers", waitingUsers);
+      });
+      socket.on("openRoomWithUser", async (payload) => {
+        const userID = payload.user._id;
+        const { user, assistant } = payload;
+        waitingUsers = waitingUsers.filter((u) => u._id !== userID);
+        const roomID = await isExistentRoom(userID, assistant._id);
 
-                await newUserConnection(payload, waitingUsers, socket.id)
-                assistants.forEach(a => {
-                    console.log(assistants.length, 'Number of online assistants')
-                    socket.to(a.assistant._id).emit('waitingUsers', waitingUsers)
-                })
-                socket.join(payload._id)
-            })
-            socket.on('newAssistant', async (payload) => {
-                const newAssistant = {
-                    assistant: payload,
-                    socketID: socket.id
-                }
-                const isAssistantOn = assistants.find(a => a.assistant._id === payload._id)
-                if(assistants.length >0){
-                    assistants.push(newAssistant)
-                    socket.join(payload._id)
-                    socket.emit('waitingUsers', waitingUsers)
-                    return
-                }
+        if (roomID) {
+          socket.join(roomID);
+          socket.emit("onUserChat", { user, roomID });
+          socket.to(userID).emit("joinChat", roomID);
+        } else {
+          const roomID = await saveRoom(userID, assistant._id);
+          socket.join(roomID);
+          socket.emit("onUserChat", { user, roomID });
+          socket.to(userID).emit("joinChat", roomID);
+        }
+      });
 
-                if (isAssistantOn) {
+      socket.on("joinSupportAssistant", (payload) => {
+        socket.join(payload);
+      });
+      socket.on("newMessage", async (payload) => {
+        const { roomID } = payload;
+        const { message } = payload;
+        const newMessage = await saveMessage(message, roomID);
 
-                } else {
-                    assistants.push(newAssistant)
-                }
+        // message can have multiples files
+        socket.emit("recipientMessage", newMessage);
+        socket.to(roomID).emit("recipientMessage", newMessage);
+      });
 
-                socket.join(payload._id)
-                socket.emit('waitingUsers', waitingUsers)
-            })
-            socket.on('openRoomWithUser', async (payload) => {
-                const userID = payload.user._id
-                const { user, assistant } = payload
-                waitingUsers = waitingUsers.filter(u => u._id !== userID)
-                const roomID = await isExistentRoom(userID, assistant._id)
-                if (roomID) {
-            
-                    socket.join(roomID)
-                    socket.emit("onUserChat", {user, roomID})
-                    socket.to(userID).emit('joinChat', roomID)
-                } else {
-                    const roomID = await saveRoom(userID, assistant._id)
-                    socket.join(roomID)
-                    socket.emit("onUserChat", {user, roomID})
-                    socket.to(userID).emit('joinChat', roomID)
-                }
-            })
-            socket.on('joinSupportAssistant', payload=>{
-       
-                socket.join(payload)
-            })
-            socket.on('newMessage', async (payload) => {
-                const { roomID } = payload
-                const { message } = payload
-                const newMessage = await saveMessage(message, roomID)
-              
-                // message can have multiples files
-                socket.emit('recipientMessage', newMessage)
-                socket.to(roomID).emit('recipientMessage', newMessage)
-         
-            })
-
-            socket.on('forceUserDisconnect', () => {
-               
-                waitingUsers = waitingUsers.filter(u => u.socketID !== socket.id)
-                assistants = assistants.filter(a => a.socketID !== socket.id)
-                assistants.forEach(a => {
-                    console.log(assistants.length, 'Number of online assistants')
-                    socket.to(a.assistant._id).emit('updateWaitingUser', waitingUsers)
-                })
-                console.log(waitingUsers.length,  'number of waiting users after forceDC')
-
-            })
-            socket.on("disconnect", () => {
-                assistants = assistants.filter(a => a.socketID !== socket.id)
-                waitingUsers = waitingUsers.filter(u => u.socketID !== socket.id)
-                assistants.forEach(a => {
-                    console.log(assistants.length, 'Number of online assistants')
-                    socket.to(a.assistant._id).emit('updateWaitingUser', waitingUsers)
-                })
-                console.log(assistants.length, 'number of Assistants after disconnect')
-            })
-        })
-    } catch (error) {
-        console.log(error)
-    }
-}
+      socket.on("forceUserDisconnect", () => {
+        waitingUsers = waitingUsers.filter((u) => u.socketID !== socket.id);
+        assistants = assistants.filter((a) => a.socketID !== socket.id);
+        assistants.forEach((a) => {
+          console.log(assistants.length, "Number of online assistants");
+          socket.to(a.assistant._id).emit("updateWaitingUser", waitingUsers);
+        });
+      });
+      socket.on("disconnect", () => {
+        assistants = assistants.filter((a) => a.socketID !== socket.id);
+        waitingUsers = waitingUsers.filter((u) => u.socketID !== socket.id);
+        assistants.forEach((a) => {
+          console.log(assistants.length, "Number of online assistants");
+          socket.to(a.assistant._id).emit("updateWaitingUser", waitingUsers);
+        });
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
