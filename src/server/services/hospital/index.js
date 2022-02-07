467f592f0f4019ebeefdda0b58a69c6f601713b1
koulinn <drdverzola@gmail.com>
2022-02-07 17:56:03 +0000

    Revert "open routes"
    This reverts commit 32d971b7e447597848ced482d8181d4f292351ce.

--- src/server/services/hospital/index.js
+++ src/server/services/hospital/index.js
@@ -20,11 +20,22 @@ router
   .route("/")
   .post(addNewTest)
   .get(searchTest)
-router.route("/medicalRequests").get(getUnconfirmedMedicalRequests);
-router.route("/medicalRequests/:requestID").put(confirmUserTests);
-router.route("/user").get(getUsers);
-router.route("/addResult/:requestID").put(addResult);
-router.route("/assistants").post(addAssistant).get(getAssistant);
+// router
+//   .route("/medicalRequests")
+//   .get(getUnconfirmedMedicalRequests)
+// router
+//   .route("/medicalRequests/:requestID")
+//   .put(confirmUserTests)
+// router
+//   .route("/user")
+//   .get(getUsers)
+// router
+//   .route("/addResult/:requestID")
+//   .put(addResult)
+// router
+//   .route("/assistants")
+//   .post(addAssistant)
+//   .get(getAssistant)
 
 
 
